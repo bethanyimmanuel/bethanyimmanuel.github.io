0efcdd00766fe75a04645094956d3365d0c4564f
Julia Han <jjh6283@rit.edu>
2024-11-11 16:35:18 -0500

    Backend and frontend communication test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,45 +5,25 @@ import HomePage from './pages/HomePage';
 import AttendancePage from './pages/AttendancePage';
 import './App.css';
 
-// function App() {
-//   return (
-//     <Router>
-//       <div>
-//         {/* Navigation Links */}
-//         <nav>
-//           <Link to="/">Home</Link>
-//           <Link to="/attendance">Attendance</Link>
-//         </nav>
-        
-//         {/* Route Definitions */}
-//         <Routes>
-//           <Route path="/" element={<HomePage />} />
-//           <Route path="/attendance" element={<AttendancePage />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    // Make a GET request to the backend API
-    fetch('https://bethanyimmanuel-4f2d5b192706.herokuapp.com/') // Replace with your Heroku app URL
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message))
-      .catch((error) => console.error('Error:', error));
-  }, []);
-
   return (
-    <div>
-      <h1>{message || 'Loading...'}</h1>
-    </div>
+    <Router>
+      <div>
+        {/* Navigation Links */}
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/attendance">Attendance</Link>
+        </nav>
+        
+        {/* Route Definitions */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/attendance" element={<AttendancePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
--- frontend/src/pages/AttendancePage.js
+++ frontend/src/pages/AttendancePage.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function AttendancePage() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    // Make a GET request to the backend API
+    fetch(`${process.env.REACT_APP_API_URL}/api/test`)
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error('Error:', error));
+  }, []);
+
   return (
     <div>
-      <h1>Attendance Page</h1>
-      {/* Add attendance tracking content here */}
+      <h1>{message || 'Loading...'}</h1>
     </div>
   );
 }
